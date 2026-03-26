df8dfd28ca5b8092b512084f46cb1c690cf8e9b9
David Paez <jeffersonpaez5@gmail.com>
2026-03-26 19:43:10 -0400

    Resolviendo Conflictos

--- EspaceX_api/App.xaml.cs
+++ EspaceX_api/App.xaml.cs
@@ -1,10 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using EspaceX_api.Services;
 using EspaceX_api.ViewModels;
+using EspaceX_api.Views;
 using System;
-using System.Globalization;
 using System.Windows;
-using System.Windows.Data;
 
 namespace EspaceX_api
 {
@@ -40,45 +39,17 @@ namespace EspaceX_api
             services.AddSingleton<MainWindow>();
         }
 
-        /// <summary>
-        /// Se ejecuta al iniciar la aplicación.
-        /// El contenedor DI inyecta automáticamente todas las dependencias.
-        /// </summary>
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
             var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
         }
 
-        /// <summary>
-        /// Se ejecuta al cerrar la aplicación.
-        /// Limpia recursos del contenedor DI.
-        /// </summary>
         protected override void OnExit(ExitEventArgs e)
         {
             _serviceProvider?.Dispose();
             base.OnExit(e);
-        }
-    }
-
-    /// <summary>
-    /// Convertidor IValueConverter para visibilidad basada en string.
-    /// Convierte un string vacío a Visibility.Collapsed.
-    /// </summary>
-    public class StringToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return string.IsNullOrWhiteSpace(value?.ToString())
-                ? Visibility.Collapsed
-                : Visibility.Visible;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
         }
     }
 }

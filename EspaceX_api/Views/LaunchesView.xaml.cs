88ccc91cc2e4ee191c5c3ecff759c3fb274a204d
Brailyn <ramirezbrailyn55@gmail.com>
2026-03-26 17:46:24 -0400

    agrege DataGrid con filtro por estado, búsqueda en tiempo real, indicador de carga y mensajes de error en dark mode"

--- EspaceX_api/Views/LaunchesView.xaml.cs
+++ EspaceX_api/Views/LaunchesView.xaml.cs
@@ -1,12 +1,14 @@
+using EspaceX_api.ViewModels;
 using System.Windows.Controls;
 
-namespace EspaceX_api
+namespace EspaceX_api.Views
 {
     public partial class LaunchesView : UserControl
     {
-        public LaunchesView()
+        public LaunchesView(LaunchesViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
